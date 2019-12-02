4898452e862d8ef39888072d118e8ef24029cf5b
neeleshdodda44 <ndodda@berkeley.edu>
2019-12-02 13:53:00 -0800

    debugging

---
+++ protocol.py
@@ -0,0 +1,169 @@
+import json
+
+from autobahn.twisted.websocket import WebSocketServerProtocol
+
+import state
+from message import Message
+from message.MessageType import REGISTER, NEW_SESSION, NEW_WEIGHTS
+from message.LibraryType import PYTHON, JS
+from coordinator import start_new_session, add_model_to_new_message
+from aggregator import handle_new_weights
+
+
+class CloudNodeProtocol(WebSocketServerProtocol):
+    """
+    Cloud Node Protocol
+
+    Class that implements part of the Cloud Node networking logic (what happens
+    when a new node connects, sends a message, disconnects). The networking here
+    happens through Websockets using the autobahn library.
+
+    """
+
+    def onConnect(self, request):
+        """
+        Logs that a node has successfully connected.
+        """
+        print("Client connecting: {}".format(request.peer))
+
+    def onOpen(self):
+        """
+        Logs that a connection was opened.
+        """
+        print("WebSocket connection open.")
+
+    def onClose(self, wasClean, code, reason):
+        """
+        Deregisters a node upon websocket closure and logs it.
+        """
+        print("WebSocket connection closed: {}".format(reason))
+        self.factory.unregister(self)
+
+    def onMessage(self, payload, isBinary):
+        """
+        Processes the payload received by a connected node.
+
+        Messages are ignored unless the message is of type "REGISTER" or the
+        node has already been registered (by sending a "REGISTER" type message).
+
+        """
+        print("Got payload!")
+        if isBinary:
+            print("Binary message not supported.")
+            return
+
+        # Convert message to JSON
+        try:
+            serialized_message = json.loads(payload)
+        except Exception:
+            print("Error while converting JSON.")
+            return
+
+        # Deserialize message
+        try:
+            message = Message.make(serialized_message)
+            print("Message ({0}) contents: {1}".format(message.type, message))
+        except Exception as e:
+            print("Error deserializing message!", e)
+            error_json = json.dumps({"error": True, "message": "Error deserializing message: {}".format(e)})
+            self.sendMessage(error_json.encode(), isBinary)
+            return
+
+        # Process message
+        if message.type == REGISTER.value:
+            # Register the node
+            if message.node_type in ["DASHBOARD", "LIBRARY"]:
+                self.factory.register(self, message.node_type)
+                print("Registered node as type: {}".format(message.node_type))
+
+                if message.node_type == "LIBRARY" and state.state["busy"] is True:
+                    # There's a session active, we should incorporate the just
+                    # added node into the session!
+                    print("Adding the new library node to this round!")
+                    last_message = state.state["last_message_sent_to_library"]
+                    if state.state["library_type"] == PYTHON.value:
+                        last_message = add_model_to_new_message(last_message)
+                    message_json = json.dumps(last_message)
+                    self.sendMessage(message_json.encode(), isBinary)
+                    # print("Session active. Registering, but not adding to this round.")
+            else:
+                print("WARNING: Incorrect node type ({}) -- ignoring!".format(message.node_type))
+        elif message.type == NEW_SESSION.value:
+            # Verify this node has been registered
+            if not self._nodeHasBeenRegistered(client_type="DASHBOARD"): return
+            # Start new DML Session
+            results = start_new_session(message, self.factory.clients["LIBRARY"])
+
+            # Error check
+            if results["error"]:
+                self.sendMessage(json.dumps(results).encode(), isBinary)
+                return
+
+            # Handle results
+            if results["action"] == "BROADCAST":
+                self._broadcastMessage(
+                    payload=results["message"],
+                    client_list=results["client_list"],
+                    isBinary=isBinary,
+                )
+
+        elif message.type == NEW_WEIGHTS.value:
+            # Verify this node has been registered
+            if not self._nodeHasBeenRegistered(client_type="LIBRARY"): return
+
+            if not self.factory.clients["DASHBOARD"]: 
+                state.state.reset()
+                print("Disconnected from dashboard client, stopping session.")
+                return
+
+            # Handle new weights (average, move to next round, terminate session)
+    
+            results = handle_new_weights(message, self.factory.clients)
+
+            # Error check
+            if results["error"]:
+                self.sendMessage(json.dumps(results).encode(), isBinary)
+                return
+
+            # Handle message
+            if "action" in results:
+                if results["action"] == "BROADCAST":
+                    self._broadcastMessage(
+                        payload=results["message"],
+                        client_list=results["client_list"],
+                        isBinary=isBinary,
+                    )
+            else:
+                # Acknowledge message (temporarily! -- node doesn't need to know)
+                self.sendMessage(json.dumps({"error": False, "message": "ack"}).encode(), isBinary)
+                message = {
+                    "session_id": state.state["session_id"],
+                    "repo_id": state.state["repo_id"],
+                    "action": "NEW_MODEL"
+                }
+                self._broadcastMessage(
+                    payload=message,
+                    client_list = self.factory.clients["DASHBOARD"],
+                    isBinary=isBinary
+                )
+        else:
+            print("Unknown message type!")
+            error_json = json.dumps({"error": True, "message": "Unknown message type!"})
+            self.sendMessage(error_json.encode(), isBinary)
+
+        print("[[DEBUG] State: {}".format(state.state))
+
+    def _broadcastMessage(self, payload, client_list, isBinary):
+        """
+        Broadcast message (`payload`) to a `client_list`.
+        """
+        for c in client_list:
+            results_json = json.dumps(payload)
+            c.sendMessage(results_json.encode(), isBinary)
+
+    def _nodeHasBeenRegistered(self, client_type):
+        """
+        Returns whether the node in scope has been registered into one of the
+        `client_type`'s.
+        """
+        return self.factory.is_registered(self, client_type)
---
+++ server.py
@@ -0,0 +1,94 @@
+import sys
+import uuid
+import json
+import io
+import os
+
+import tensorflow as tf
+from flask_cors import CORS, cross_origin
+from twisted.python import log
+import werkzeug.formparser
+from twisted.web.server import Site
+from twisted.web.wsgi import WSGIResource
+from twisted.internet import task, reactor
+from flask import Flask, jsonify, send_from_directory
+from autobahn.twisted.resource import WebSocketResource, WSGIRootResource
+
+from protocol import CloudNodeProtocol
+from factory import CloudNodeFactory
+from message.LibraryType import PYTHON, JS
+import state
+
+
+app = Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+CORS(app)
+
+@app.route("/status")
+def get_status():
+    """
+    Returns the status of the Cloud Node.
+
+    The dashboard-api is the only hitting this endpoint, so it should be secured.
+    """
+    return jsonify({"Busy": state.state["busy"]})
+
+@app.route('/model/<path:filename>')
+def serve_tfjs_model(filename):
+    """
+    Serves the TFJS model to the user.
+
+    TODO: Should do this through ngnix for a boost in performance. Should also
+    have some auth token -> session id mapping (security fix in the future).
+
+    Args:
+        filename (str): The filename to serve.
+    """
+    if not state.state["busy"]:
+        return "No active session!\n"
+
+    if state.state["library_type"] != JS.value:
+        return "Current session is not for JAVASCRIPT!"
+
+    return send_from_directory(
+        os.path.join(app.root_path, state.state['tfjs_model_path']),
+        filename,
+    )
+
+@app.route('/secret/reset_state')
+def reset_state():
+    """
+    Resets the state of the cloud node.
+
+    TODO: This is only for debugging. Should be deleted.
+    """
+    state.state_lock.acquire()
+    state.reset_state()
+    state.state_lock.release()
+    return "State reset successfully!\n"
+
+@app.route('/secret/get_state')
+def get_state():
+    """
+    Get the state of the cloud node.
+
+    TODO: This is only for debugging. Should be deleted.
+    """
+    return repr(state.state)
+
+if __name__ == '__main__':
+    tf.compat.v1.disable_eager_execution()
+    log.startLogging(sys.stdout)
+
+    factory = CloudNodeFactory()
+    factory.protocol = CloudNodeProtocol
+    wsResource = WebSocketResource(factory)
+
+    wsgiResource = WSGIResource(reactor, reactor.getThreadPool(), app)
+    rootResource = WSGIRootResource(wsgiResource, {b'': wsResource})
+    site = Site(rootResource)
+
+    state.init()
+
+    reactor.listenTCP(8999, site)
+    reactor.run()
